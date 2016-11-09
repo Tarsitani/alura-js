ca30283dbe840ba75408bccd676b288b6e86c32a
Lucas Reis <lucas.reis@strategymanager.com.br>
2016-11-09 17:11:31 -0200

    botão adiciona DONE!

---
+++ projeto/adiciona.js
@@ -0,0 +1,30 @@
+// var table - document.getElementsByTagName("table")[0];
+
+// var table = document.querySelectorAll("table"); //pega todos os elementos "table" e coloca em um array
+
+
+
+var botao = document.querySelector("#adicionar-paciente");
+botao.addEventListener("click", function(event){
+
+    event.preventDefault(); //impede comportamento padrão
+
+    var campoNome = document.querySelector("#campo-nome");
+    var campoPeso = document.querySelector("#campo-peso");
+    var campoAltura = document.querySelector("#campo-altura");
+
+
+    var pacienteNovo = "<tr class='paciente'>"+
+                        "<td class='info-nome'>"+ campoNome.value +"</td>"+
+                        "<td class='info-peso' id='peso-1'>"+ campoPeso.value +"</td>"+
+                        "<td class='info-altura' id='altura-1'>"+ campoAltura.value +"</td>"+
+                        "<td class='info-imc' id='imc-1'></td>"+
+                    "</tr>";
+
+    var tabela = document.querySelector("table"); //pega só o primeiro elemento
+    tabela.innerHTML += pacienteNovo;//adiciona o elemento HTML com o .innerHTML
+
+    campoNome.value = "";
+    campoPeso.value = "";
+    campoAltura.value = "";
+});
